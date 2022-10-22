6bec989fad1250689cd6328838d9b3b760d582ea
77Sun77 <84917607+77Sun77@users.noreply.github.com>
2022-10-23 00:04:41 +0900

    Back up

--- Assets/02.Scripts/Gimmicks/Wall_ColorReflect.cs
+++ Assets/02.Scripts/Gimmicks/Wall_ColorReflect.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class Wall_ColorReflect : MonoBehaviour
 {
     public  Ball.Ball_Color ball_Color;
+
+    public enum WallKind { Other, ColorReflect };
+    public WallKind kind;
+
     [SerializeField]
     Material[] materials;
     private void Start()
@@ -20,9 +24,13 @@ public class Wall_ColorReflect : MonoBehaviour
 
     private void OnCollisionEnter(Collision coll)
     {
-
         if (coll.gameObject.CompareTag("BALL"))
         {
+            if (kind == WallKind.ColorReflect && gameObject.layer - coll.gameObject.layer == 9)
+            {
+                Physics.IgnoreLayerCollision(gameObject.layer, coll.gameObject.layer, true);
+                return;
+            }
             Rigidbody ballRigid = coll.gameObject.GetComponent<Rigidbody>();
             Vector3 velocity = coll.gameObject.GetComponent<Ball>().velocity;
             ballRigid.velocity = Vector3.Reflect(velocity, -coll.GetContact(0).normal);
@@ -30,5 +38,5 @@ public class Wall_ColorReflect : MonoBehaviour
 
 
     }
-
+    
 }
